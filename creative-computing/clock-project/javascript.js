c6dda5ba5256e6b162be51e690df836caab5db9d
DotsA <abesf518@newschool.edu>
2017-10-31 18:16:36 -0400

    update

--- creative-computing/clock-project/javascript.js
+++ creative-computing/clock-project/javascript.js
@@ -1,22 +1,30 @@
 function updateTime() {
 
 	var clockHolder = document.getElementById("clock")
+	var body = document.getElementById("body")
+	var backcolor = document.getElementById("background-color")
+	
 
 	var now = new Date()
 	var seconds = now.getSeconds()
+	var minutes = now.getMinutes()
+	var hours = now.getHours()
+	
+
 	now = now.toTimeString()
+        
 
+    clockHolder.setAttribute("src", seconds + "c.jpg");
+    
 
-	if (seconds < 20) {
-		//red
-		clockHolder.style.color = "red"
-	} else if (seconds < 40){
-	   //green
-	   clockHolder.style.color = "green"
-	} else { 
-		//blue
-		clockHolder.style.color = "blue"
-	}
+       if (minutes > 1) {
+	    clockHolder.style.color = "blue";
+     	//change image	
+	    }
+
+	    else if (true) { 
+       clockHolder.style.color = "pink"
+	   }
 
 	clockHolder.innerHTML = now
 }
